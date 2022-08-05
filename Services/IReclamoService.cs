2774c3fd496639874ffdbf93ae18fd0c6419850b
Juan Fabro <69061114+fabrojuan@users.noreply.github.com>
2022-08-05 03:22:29 +0200

    separacion en capas para recurso Reclamos

---
+++ Services/IReclamoService.cs
@@ -0,0 +1,14 @@
+﻿using MVPSA_V2022.clases;
+
+namespace MVPSA_V2022.Services
+{
+    public interface IReclamoService
+    {
+        public IEnumerable<TipoReclamoCLS> listarTiposReclamo();
+
+        public int guardarReclamo(ReclamoCLS reclamoCLS);
+
+        public IEnumerable<ReclamoCLS> listarReclamos();
+
+    }        
+}
