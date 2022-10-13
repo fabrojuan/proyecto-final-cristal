ba97ced8fe5136f68ed971b9393ad7a90a5071a4
Juan Fabro <69061114+fabrojuan@users.noreply.github.com>
2022-10-13 04:42:09 +0200

    seteo de header con el id de usuario

--- Interceptors/RequestUserMiddleware.cs
+++ Interceptors/RequestUserMiddleware.cs
@@ -18,21 +18,23 @@ namespace MVPSA_V2022.Interceptors
             // Obtengo el token de seguridad
             string token = context.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
 
-            if (token == null || token == "") {
+            if (token == null || token == "")
+            {
                 // Si no se envio un token (por ejemplo si es un endpoint que no lo requiere), sigue
                 // el flujo normal
                 await _next(context);
-            }
+            } else 
+            {
+                // con el token obtengo el id de usuario
+                var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
+                string usuarioId = jwt.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
 
-            // con el token obtengo el id de usuario
-            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
-            string usuarioId = jwt.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
+                // seteo el id de usuario como un header
+                context.Request.Headers.Add("id_usuario", usuarioId);
 
-            // seteo el id de usuario como un header
-            context.Request.Headers.Add("id_usuario", usuarioId);
-
-            // ese nuevo header "id_usuario" llega al controlador
-            await _next(context);
+                // ese nuevo header "id_usuario" llega al controlador
+                await _next(context);
+            }            
         }
     }
 }
