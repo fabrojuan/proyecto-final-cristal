5a5297f7ce641bc141c06e238994f1567eea579b
Juan Fabro <69061114+fabrojuan@users.noreply.github.com>
2024-05-28 23:43:03 -0300

    VPSA-215

--- clases/CrearReclamoRequestDto.cs
+++ clases/CrearReclamoRequestDto.cs
@@ -4,9 +4,9 @@
     {
         public string descripcion { get; set; }
         public int codTipoReclamo { get; set; }
-        public string calle { get; set; }
-        public string altura { get; set; }
-        public string entreCalles { get; set; }
+        public string? calle { get; set; }
+        public string? altura { get; set; }
+        public string? entreCalles { get; set; }
         public string? NomApeVecino { get; set; }
         public string? MailVecino { get; set; }
         public string? TelefonoVecino { get; set; }
